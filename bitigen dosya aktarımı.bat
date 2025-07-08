19ea292399c0911069d5513ec001616252d4ba9a
unknown <admin@izgiteknoloji.com>
2025-07-08 18:43:49 +0300

    Otomatik y├╝kleme - 08.07.2025 18:43:49,04

--- bitigen dosya aktarımı.bat
+++ bitigen dosya aktarımı.bat
@@ -11,3 +11,45 @@ if %errorlevel%==0 (
     echo Değişiklik yok, işlem yapılmadı.
 )
 del temp_gitstatus.txt
+
+@echo off
+cd /d "C:\Users\Hp\Documents\GITHUB\İzgi Enerji"
+git status --porcelain > temp_gitstatus.txt
+findstr /R "." temp_gitstatus.txt >nul
+if %errorlevel%==0 (
+    git add .
+    git commit -m "Otomatik yükleme - %DATE% %TIME%"
+    git push origin main
+    echo Değişiklikler yüklendi.
+) else (
+    echo Değişiklik yok, işlem yapılmadı.
+)
+del temp_gitstatus.txt
+
+@echo off
+cd /d "C:\Users\Hp\Documents\GITHUB\İzgi Teknoloji"
+git status --porcelain > temp_gitstatus.txt
+findstr /R "." temp_gitstatus.txt >nul
+if %errorlevel%==0 (
+    git add .
+    git commit -m "Otomatik yükleme - %DATE% %TIME%"
+    git push origin main
+    echo Değişiklikler yüklendi.
+) else (
+    echo Değişiklik yok, işlem yapılmadı.
+)
+del temp_gitstatus.txt
+
+@echo off
+cd /d "C:\Users\Hp\Documents\GITHUB\İzgi Yakıt"
+git status --porcelain > temp_gitstatus.txt
+findstr /R "." temp_gitstatus.txt >nul
+if %errorlevel%==0 (
+    git add .
+    git commit -m "Otomatik yükleme - %DATE% %TIME%"
+    git push origin main
+    echo Değişiklikler yüklendi.
+) else (
+    echo Değişiklik yok, işlem yapılmadı.
+)
+del temp_gitstatus.txt
